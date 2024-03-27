17a5a0410242f75fcf0f556e9c975cdbefe290c7
team5cty <team5cty7@gmail.com>
2024-03-27 16:33:10 +0530

    docker

--- example_output_module/handlers/Users.go
+++ example_output_module/handlers/Users.go
@@ -1,28 +1,28 @@
 package handlers
 
 import (
+	"context"
 	"encoding/json"
 	"io"
 	"net/http"
-	"context"
-	
+
 	"example_output_module/prisma/db"
 )
 
 type Users struct {
-	Email string   `json:"email"`
-	Username string   `json:"username"`
+	Email    string `json:"email"`
+	Username string `json:"username"`
 }
 type Users_list []*Users
 
-
 func (users *Users_list) ToJSON(w io.Writer) error {
-	e:= json.NewEncoder(w)
+	e := json.NewEncoder(w)
 	return e.Encode(users)
 }
 
-func GET_Users_Handler (w http.ResponseWriter, r *http.Request) {
-	client := db.NewClient() 
+func GET_Users_Handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	client := db.NewClient()
 	ctx := context.Background()
 	if err := client.Prisma.Connect(); err != nil {
 		panic(err)
@@ -31,7 +31,7 @@ func GET_Users_Handler (w http.ResponseWriter, r *http.Request) {
 		if err := client.Prisma.Disconnect(); err != nil {
 			panic(err)
 		}
-	}() 
+	}()
 	var users Users_list
 	res, err := client.User.FindMany().Exec(ctx)
 	if err != nil {
@@ -40,7 +40,7 @@ func GET_Users_Handler (w http.ResponseWriter, r *http.Request) {
 	}
 	for _, object := range res {
 		ele := &Users{
-			Email: object.Email,
+			Email:    object.Email,
 			Username: object.Username,
 		}
 		users = append(users, ele)
