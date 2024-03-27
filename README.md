7041c161872838275955cf224edf14915e2107a1
team5cty <team5cty7@gmail.com>
2024-03-27 20:54:25 +0530

    Working Dockerfile

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 # microservice_template
-w.Header().Set("Content-Type", "application/json")
-var {{.endpoint.Name | tolower}} {{.endpoint.Name}}
-// Implement logic for {{.endpoint.Method}} {{.endpoint.Path}}
-{{.endpoint.Name | tolower}}.ToJSON(w)
--- example_output_module/handlers/AddUser.go
+++ example_output_module/handlers/AddUser.go
@@ -1,55 +1,47 @@
 package handlers
 
 import (
-	"context"
 	"encoding/json"
 	"io"
 	"net/http"
-
+	"context"
+	
 	"example_output_module/prisma/db"
 )
 
 type AddUser struct {
-	Dob      string `json:"dob"`
-	Email    string `json:"email"`
-	Id       int    `json:"id"`
-	Username string `json:"username"`
+	Dob string   `json:"dob"`
+	Email string   `json:"email"`
+	Id int   `json:"id"`
+	Username string   `json:"username"`
 }
 
-// FromJSON decodes JSON data from an io.Reader into an AddUser struct
+
 func (adduser *AddUser) FromJSON(r io.Reader) error {
-	d := json.NewDecoder(r)
+	d:= json.NewDecoder(r)
 	return d.Decode(adduser)
 }
 
-// POST_AddUser_Handler handles HTTP POST requests to add a new user
-func POST_AddUser_Handler(w http.ResponseWriter, r *http.Request) {
-	client := db.NewClient()
+func POST_AddUser_Handler (w http.ResponseWriter, r *http.Request) {
+	client := db.NewClient() 
 	ctx := context.Background()
-
-	// Connect to the database
 	if err := client.Prisma.Connect(); err != nil {
-		http.Error(w, "Failed to connect to the database", http.StatusInternalServerError)
-		return
+		panic(err)
 	}
 	defer func() {
-		// Disconnect from the database
 		if err := client.Prisma.Disconnect(); err != nil {
-			http.Error(w, "Failed to disconnect from the database", http.StatusInternalServerError)
+			panic(err)
 		}
 	}()
 
-	// Set response content type
 	w.Header().Set("Content-Type", "application/json")
-
-	// Decode request body into AddUser struct
+	
 	var requestData AddUser
 	if err := requestData.FromJSON(r.Body); err != nil {
 		http.Error(w, "Failed to decode request body", http.StatusBadRequest)
 		return
 	}
-
-	// Create new user in the database
+	
 	_, err := client.User.CreateOne(
 		db.User.Dob.Set(requestData.Dob),
 		db.User.Email.Set(requestData.Email),
@@ -58,10 +50,7 @@ func POST_AddUser_Handler(w http.ResponseWriter, r *http.Request) {
 	).Exec(ctx)
 
 	if err != nil {
-		http.Error(w, "Failed to create user: "+err.Error(), http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
-	// Write success response
-	w.WriteHeader(http.StatusCreated)
 }
